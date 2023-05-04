e5b7277d7b36920444f3db5eb4e8953ad4be7c5c
Cody Koscielski <codykoscielski@gmail.com>
2023-05-03 21:16:30 -0400

    Learned about proper headers, footers, and WP Admin bar

--- footer.php
+++ footer.php
@@ -1,1 +1,4 @@
 <h1>footer.php</h1>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+    function uni_files() {
+        wp_enqueue_style('uni_main_styles', get_stylesheet_uri());
+    }
+
+    add_action('wp_enqueue_scripts', 'uni_files');
